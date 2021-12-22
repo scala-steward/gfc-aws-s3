f1daa6d8d2fcaa541d7e4bdf5d6c3b3a3e2d1562
Sean C. Sullivan <github@seansullivan.com>
2021-12-22 07:59:47 -0800

    s3mock 2.3.2

--- src/test/scala/org/gfccollective/aws/s3/akka/IntegrationSpec.scala
+++ src/test/scala/org/gfccollective/aws/s3/akka/IntegrationSpec.scala
@@ -22,7 +22,7 @@ class IntegrationSpec extends AnyWordSpec
   with Matchers
   with BeforeAndAfterEach {
 
-  private val dockerImageName = DockerImageName.parse(S3MockContainer.IMAGE_NAME).withTag("2.2.3")
+  private val dockerImageName = DockerImageName.parse(S3MockContainer.IMAGE_NAME).withTag("2.3.2")
   private val region = "us-east-1"
   private val awsCredentials: AWSCredentials = new BasicAWSCredentials("aaa", "bbb")
   private var bucketName: String = _
